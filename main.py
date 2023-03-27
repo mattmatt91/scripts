c5d4f1e31e711a13201160ec55e6a51306b11861
mattmatt91 <matthias_muhr@web.de>
2023-03-27 14:13:41 +0200

    format

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from filereader.read_files import scan_folder
 from helpers.helpers import Helpers as hp
-from multistatistics.do_statistics import calculate
+from multistatistics.do_statistics import do_statistics
 from compare.compare_measurements import compare
 from plots.plot_feauters import plot_features
 
+
+root_path = 'D:\\test_dataset'
+# root_path = 'D:\\safe_combustion'
+
+
 if __name__ == '__main__':
-    root_path = 'D:\\test_dataset'
-    # root_path = 'C:\\Users\\mmuhr-adm\\Desktop\\02.03.2023'
     properties = hp.read_json('properties', 'properties.json')
     # scan_folder(root_path, properties)
-    calculate(root_path, properties, statistic=False, pca=True, lda=True) # computing statistics
-    # compare(root_path, properties)
-    # plot_features(root_path, properties) # plots feauteres with all samples
+    # do_statistics(root_path, properties, statistic=True,pca=True, lda=True)  # computing statistics
+    compare(root_path, properties)
+    exit()
+    plot_features(root_path, properties)  # plots feauteres with all samples
     print('finished')
