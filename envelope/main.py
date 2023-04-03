c42ee6b88f1d449dc1727473334292ef66328436
mattmatt91 <matthias_muhr@web.de>
2023-04-03 09:41:12 +0200

    update envelope

--- envelope/main.py
+++ envelope/main.py
@@ -1,31 +1,27 @@
 import numpy as np
-from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 from read import ReadFile
+from scipy.signal import find_peaks
+from numpy.polynomial import Polynomial
 
 
 
 data, time = ReadFile.read('data.csv')
 
-# Generate noisy 2D data
-# t = np.linspace(0, 1, 100)
-# x = np.sin(10 * np.pi * t) + 0.2 * np.random.randn(100)
-# y = np.sin(5 * np.pi * t) + 0.2 * np.random.randn(100)
-
-y = data
-t= time
-# Calculate upper envelope
-peaks, _ = find_peaks(y)
-print(len(peaks))
-print(len(data))
-# upper_envelope = np.interp(t, t[peaks], y[peaks])
-
-# Plot results
-plt.plot(t, y, label='Data')
-plt.plot(peaks, data[peaks], label='Noisy Data')
-# plt.plot(t, upper_envelope, label='Upper Envelope')
-# plt.legend()
-plt.show()
+def calc_envelope(data, time):
+    x_peaks, _ = find_peaks(data, prominence=0.3, distance=50)
+    y_peaks = data[x_peaks]
+    
+    
+    plt.plot(data)
+    plt.plot(x_peaks, y_peaks)
+    plt.plot(x_peaks, data[x_peaks], "o")
+    plt.show()
 
 
+def polynomial_fit(x, y, degree):
+    p = Polynomial.fit(x, y, degree)
+    return p
 
+
+calc_envelope(data, time)
