f1e57ed13247a37f87f66909b6b90833ca696ba6
mattmatt91 <matthias_muhr@web.de>
2023-04-20 13:50:17 +0200

    _

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ root_path = 'J:\\safe_combustion'
 
 if __name__ == '__main__':
     os.environ["DATA_PATH"] = root_path
-    # scan_folder()  # reads all measurement and evaluates them
+    scan_folder()  # reads all measurement and evaluates them
     do_statistics(statistic=True, pca=True, lda=True)# processes statisctics
-    do_machine_learning() # 
+    # do_machine_learning() 
     compare()  # does some plots of sensor signals
-    # plot_features()  # plots feauteres with all samples
+    plot_features()  # plots feauteres with all samples
     print('finished')
 
 
+
+# new samples missung in sensor plot
