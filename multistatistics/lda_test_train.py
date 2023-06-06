a434b5963e996e32c06b72262ff6f3fde2e11e2d
mattmatt91 <matthias_muhr@web.de>
2023-06-06 21:55:49 +0200

    _

--- multistatistics/lda_test_train.py
+++ multistatistics/lda_test_train.py
@@ -3,7 +3,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-from random import uniform, randint
+from random import uniform, randint, random
+import pandas as pd
+from sklearn.decomposition import PCA
+import numpy as np
 from datetime import datetime, timedelta
 
 
@@ -26,23 +29,28 @@ def predict(lda: LDA, x_test: pd.DataFrame, y_test: pd.Series, sample_dict: dict
     return lda.transform(x_test)
 
 
-def prepare_data(file_path: str):
-    print(file_path)
-    df = pd.read_csv(file_path, delimiter=';', decimal=',')
-    df.fillna(0)  # features without values filled with 0.
-    info_cols = ['datetime',
-                 'height',
-                 'number',
-                 'sample',
-                 'name',
-                 'ball',
-                 'rate',
-                 'combustion',
-                 'combustion_bool']
-    infos = df[info_cols]
-    features = df.drop(columns=info_cols)
-    features.index = infos['name']
-    return features, infos
+def get_datetime(date_string:str, minutes_to_add:int):
+    input_format = "%d-%m-%Y_%H-%M-%S"
+    output_format = "%d-%m-%Y_%H-%M-%S"
+    datetime_obj = datetime.strptime(date_string, input_format)
+    datetime_obj += timedelta(minutes=minutes_to_add)
+    new_date_string = datetime_obj.strftime(output_format)
+    return new_date_string
+
+def get_number(old_number:int, counter:int):
+    num = 100*old_number + counter
+    return num
+
+def get_name(old_name:str, counter:int):
+    name = old_name.split('_')[0]
+    try:
+        number = int(old_name.split('_')[1])
+    except:
+        number = int(old_name.split('_')[2])
+
+    new_number = 100*number + counter
+    new_name = f'{name}_{new_number}'
+    return new_name
 
 
 def sample_to_numbers(samlpes: pd.Series):
@@ -64,7 +72,7 @@ def get_key_by_value(data: dict, value: int):
             return key
 
 
-def dont_tell_mom(file_path: str, num_new: int, path_out: str):
+def load_data(file_path: str):
     df = pd.read_csv(file_path, delimiter=';', decimal=',')
     df.fillna(0)  # features without values filled with 0.
     info_cols = ['datetime',
@@ -78,61 +86,52 @@ def dont_tell_mom(file_path: str, num_new: int, path_out: str):
                  'combustion_bool']
     infos = df[info_cols]
     features = df.drop(columns=info_cols)
-    samples = infos['sample'].unique()
-    new_data = []
-    for sample in samples:
-        number = randint(1000, 1100)
-        means = features.mean()
-        stad = features.std()
-        info_old = infos[infos['sample'] == sample].iloc[0].to_dict()
-        for i in range(num_new):
-            new_features = {}
-            for feature in features:
-                new_features[feature] = means[feature]  + means[feature]*uniform(0,0.1)
-                     
-            for key in info_old:
-                if key == 'name':
-                    new_features[key] = info_old[key].split(
-                        '_')[0] + f'_{number +i}'
-                if key == 'number':
-                    new_features[key] = number + i
-                if key == 'datetime':
-                    datetime_object = datetime.strptime(
-                        info_old[key], '%d-%m-%Y_%H-%M-%S')
-                    datetime_object = datetime_object + \
-                        timedelta(minutes=i*2) + \
-                        timedelta(seconds=randint(0, 30))
-                    new_features[key] = datetime.strftime(
-                        datetime_object, '%d-%m-%Y_%H-%M-%S')
-                else:
-                    new_features[key] = info_old[key]
+    features.set_index(infos['sample'], inplace=True)
+    return infos, features
 
-            new_data.append(new_features)
-    # features.index = infos['name']
-    df = pd.DataFrame(new_data)
-    print(path_out)
-    df.to_csv(path_out, decimal=',', sep=';', index=False)
-    return path_out
+
+def generate_data(infos:pd.DataFrame, features:pd.DataFrame, num_new:int):
+    # do PCA
+    pca = PCA(n_components=3) 
+    transformed_data = pca.fit_transform(features)
+    df_transformed_data = pd.DataFrame(transformed_data, index=features.index)
+
+    # generate data
+    new_data = []
+    new_data_infos = []
+    labels = []
+    for class_label in features.index.unique():
+        mean = df_transformed_data[df_transformed_data.index == class_label].mean(
+        )
+        std = df_transformed_data[df_transformed_data.index ==
+                                  class_label].std()
+        for i in range(num_new):
+            new_measurement = (mean + std * \
+                [randint(0, 10)/10 for i in range(3)]).tolist()
+            new_data.append(new_measurement)
+            labels.append(class_label)
+            new_info_template = infos[infos['sample'] == class_label].iloc[0]
+            for key in new_info_template.index:
+                if key == 'name':
+                    new_info_template['number'] = get_number(new_info_template['number'], i)
+                elif key == 'number':
+                    new_info_template['name'] = get_name(new_info_template['name'], i)
+                elif key == 'datetime':
+                    new_info_template['datetime'] = get_datetime(new_info_template['datetime'], i)
+            new_data_infos.append(new_info_template.tolist())
+    inverse_transformed_data = pca.inverse_transform(new_data)
+    new_df = pd.DataFrame(inverse_transformed_data, columns=features.columns)
+    new_df_infos = pd.DataFrame(new_data_infos, columns=infos.columns)
+    merged_df = pd.concat([new_df_infos, new_df], axis=1)
+    merged_df.to_csv('fake_results.csv', decimal=',', sep=';')
+
 
 
 if __name__ == '__main__':
-    path_test = ":D\\test_dataset\\results\\results.csv"
-    path_train = "D:\\safe_combustion\\results\\results.csv"
-    path_new_test = "D:\\comparison_8_10\\results\\results.csv"
-    path_test = dont_tell_mom(path_train, 100, path_new_test)
+    # path_test = "results.csv"
+    path_train = "results.csv"
 
-    exit()
 
-    features_test, infos_test = prepare_data(path_test)
-    features_train, infos_train = prepare_data(path_train)
+    infos, features = load_data(path_train)
 
-    features_test = features_test.T.sort_index().T
-    features_train = features_train.T.sort_index().T
-
-    sample_dict_train, sample_to_numbers_train = sample_to_numbers(
-        infos_train['sample'])
-    sample_to_numbers_test = sample_test_to_numbers(
-        sample_dict_train, infos_test['sample'])
-    lda, train_transform = do_lda(features_train, sample_to_numbers_train)
-    test_transform = predict(
-        lda, features_test, infos_test['sample'], sample_dict_train)
+    generate_data(infos, features, 10)
--- start.ps1
+++ start.ps1
@@ -4,4 +4,4 @@ venv\scripts\activate
 
 
 python .\multistatistics\lda_test_train.py
-python main.py -p D:\comparison_8_10
+python main.py -p C:\Users\matth\Desktop\GitHub\scripts\data_test
